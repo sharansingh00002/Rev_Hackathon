11441ff50b90a1251146293e2c6ca9ffb3602066
shubham <gshubham030@gmail.com>
2020-01-18 19:36:20 +0530

    tts added

--- lib/detailed_page.dart
+++ lib/detailed_page.dart
@@ -1,14 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
-class DetailedPage extends StatelessWidget {
+class DetailedPage extends StatefulWidget {
   final text;
 
   const DetailedPage({Key key, this.text}) : super(key: key);
+
+  @override
+  _DetailedPageState createState() => _DetailedPageState();
+}
+
+enum TtsState { playing, stopped }
+
+class _DetailedPageState extends State<DetailedPage> {
+  FlutterTts flutterTts;
+  dynamic languages;
+  String language;
+  double volume = 0.5;
+  double pitch = 1.0;
+  double rate = 0.5;
+  String text;
+  TtsState ttsState = TtsState.stopped;
+
+  get isPlaying => ttsState == TtsState.playing;
+
+  get isStopped => ttsState == TtsState.stopped;
+
+  @override
+  void initState() {
+    text = widget.text.trim();
+    // TODO: implement initState
+    super.initState();
+    initTts();
+  }
+
+  initTts() {
+    flutterTts = FlutterTts();
+
+    _getLanguages();
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        print("playing");
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        print("error: $msg");
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future _getLanguages() async {
+    languages = await flutterTts.getLanguages;
+    if (languages != null) setState(() => languages);
+  }
+
+  Future _speak() async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+
+    if (text != null) {
+      if (text.isNotEmpty) {
+        var result = await flutterTts.speak(text);
+        if (result == 1) setState(() => ttsState = TtsState.playing);
+      }
+    }
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Hindi Page'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.play_circle_filled),
+            onPressed: _speak,
+          ),
+          IconButton(
+            icon: Icon(Icons.stop),
+            onPressed: _stop,
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
